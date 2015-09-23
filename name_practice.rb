65b33ce89212257c2de324d2af91ee52f39e1095
Your Name <k.krainson@gmail.com>
2015-09-22 21:49:17 -0700

    practice

---
+++ name_practice.rb
@@ -0,0 +1,14 @@
+puts "how many letters are in your name?"
+name = gets.chomp
+
+puts name.length
+
+puts "Your name spelled backwards is ..."
+
+puts name.reverse.downcase
+
+puts " How many letters are in your name?"
+answer = gets.chomp.to_i
+
+puts " Your name times two is ..."
+puts answer * answer
